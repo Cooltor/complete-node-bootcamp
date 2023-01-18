4bfc6ce1dffffe9f237a4640f30d94e8b14e8ac6
Cooltor <romaindbus86@gmail.com>
2023-01-18 10:27:32 +0100

    creating doc in DB and refactoring code

--- 4-natours/starter/routes/tourRoutes.js
+++ 4-natours/starter/routes/tourRoutes.js
@@ -3,7 +3,7 @@ const tourController = require('./../controllers/tourController.js');
 
 const router = express.Router();
 
-router.param('id', tourController.checkID);
+//router.param('id', tourController.checkID);
 
 router
   .route('/')
