dda55a2e0edbd46e7f0f179600c770aa8fa9ec76
Cooltor <romaindbus86@gmail.com>
2023-01-19 15:16:20 +0100

    mongoose query filters

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -3,14 +3,72 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD THE QUERY
+    // 1.1. Filtering
+    const queryObj = { ...req.query }; // nous devons structurer et mettre ds un objet
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
 
+    // 1.2. Advance filtering
+
+    let queryStr = JSON.stringify(queryObj); // expression régulière pour trouver exactement ces valeurs. le "g" sert à trouver tte les valeurs et pas que la 1ère occurence
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`); // ${lavaleur} et rajoute le $ devant
+    console.log(JSON.parse(queryStr));
+
+    console.log(req.query);
+
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2. Sorting
+
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+      // sort('price ratings')
+    } else {
+      query = query.sort('-createdAt'); // tri par défaut par dernier créé
+    }
+
+    // 3. Field limiting
+
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v'); // - pour exclure une ligne, là __v
+    }
+
+    // 4. Pagination
+
+    const page = req.query.page * 1 || 1; // le * 1 : astuce pr transformer str en Num
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
+    // EXECUTE THE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'succes',
       results: tours.length,
       data: {
         tours: tours,
       },
+
+      // query.sort().select().skip().limit()
+
+      //const query = Tour.find()
+      // .where('duration')
+      //.equals(5)
+      //.where('difficulty')
+      //.equals('easy');
     });
   } catch (err) {
     res.status(404).json({
--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
@@ -14,6 +15,7 @@ mongoose
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
+    useUnifiedTopology: true,
   })
   .then((con) => console.log('DB connection successfull 👌!'));
 
