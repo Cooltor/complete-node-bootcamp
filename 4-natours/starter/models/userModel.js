c11d67fe946377bde1c962490bbaa70f89d994ab
Cooltor <romaindbus86@gmail.com>
2023-01-27 16:17:13 +0100

    user sign up first commit

---
+++ 4-natours/starter/models/userModel.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name'],
+    trim: true,
+  },
+
+  email: {
+    type: String,
+    required: [true, 'Please tell us your email'],
+    unique: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+    lowercase: true,
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: [8, 'a password must have minimum 8 characters'],
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      // This only on CREATE AND SAVE!!!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Password are not the same',
+    },
+  },
+});
+
+userSchema.pre('save', async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete passwordConfirm
+  this.passwordConfirm = undefined;
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
