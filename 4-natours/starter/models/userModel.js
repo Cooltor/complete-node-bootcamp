301fc29e5e78117ab2b409d37443a3d0c6677f4c
Cooltor <romaindbus86@gmail.com>
2023-01-30 10:23:34 +0100

    first user sing up and login

--- 4-natours/starter/models/userModel.js
+++ 4-natours/starter/models/userModel.js
@@ -21,6 +21,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide a password'],
     minlength: [8, 'a password must have minimum 8 characters'],
+    select: false,
   },
   passwordConfirm: {
     type: String,
@@ -46,6 +47,15 @@ userSchema.pre('save', async function (next) {
   this.passwordConfirm = undefined;
   next();
 });
+
+// Instance method
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 
