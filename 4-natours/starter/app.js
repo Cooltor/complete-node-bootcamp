c976588c6564390a6876c7194ceec4292ed4751d
Cooltor <romaindbus86@gmail.com>
2023-02-01 16:29:47 +0100

    cookies headers secure dep

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -31,8 +34,28 @@ app.use('/api', limiter);
 // Body Parser, reading data from the body into req.body
 app.use(express.json({ limit: '10kb' }));
 
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitaziration against XSS
+app.use(xss());
+
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
 
 // Test middleware
 app.use((req, res, next) => {
