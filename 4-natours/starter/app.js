c9c5c954f5cd25e02460c15a02dea6084fb221d8
Cooltor <romaindbus86@gmail.com>
2023-02-07 11:44:21 +0100

    pug base first

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -14,7 +15,13 @@ const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 // 1. GLOBAL MIDDLEWARES
+
+// Serving static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Set Security HTTP Headers
 app.use(helmet());
@@ -42,7 +49,7 @@ app.use(mongoSanitize());
 app.use(xss());
 
 // Serving static files
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Prevent parameter pollution
 app.use(
@@ -65,6 +72,13 @@ app.use((req, res, next) => {
 });
 
 // 3. ROUTES
+app.get('/', (req, res) => {
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Jonas',
+  });
+});
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
