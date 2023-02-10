e75f3d2889b61f4855859203e9bb591108c4e5e8
Cooltor <romaindbus86@gmail.com>
2023-02-10 16:04:40 +0100

    update user password

--- 4-natours/starter/public/js/index.js
+++ 4-natours/starter/public/js/index.js
@@ -1,12 +1,13 @@
 /* eslint-disable */
 import '@babel/polyfill';
 import { login, logout } from './login';
-import { updateData } from './updateSettings';
+import { updateSettings } from './updateSettings';
 
 // DOM ELEMENTS
 const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const UserDataForm = document.querySelector('.form-user-data');
+const UserPasswordForm = document.querySelector('.form-user-password');
 
 // DELEGATION
 
@@ -26,6 +27,24 @@ if (UserDataForm) {
     e.preventDefault();
     const email = document.getElementById('email').value;
     const name = document.getElementById('name').value;
-    updateData(name, email);
+    updateSettings({ name, email }, 'data');
+  });
+}
+
+if (UserPasswordForm) {
+  UserPasswordForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    document.querySelector('.btn--save-password').textContent = 'Updating...';
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+    await updateSettings(
+      { passwordCurrent, password, passwordConfirm },
+      'password'
+    );
+    document.querySelector('.btn--save-password').textContent = 'Save password';
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
   });
 }
